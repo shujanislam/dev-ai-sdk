5e4278f5ba84ac36cfbbdb1a4676feeed7457ccd
shujanislam <shujanm.islam231@gmail.com>
2026-01-20 01:14:55 +0530

    added better error type

--- src/core/error.ts
+++ src/core/error.ts
@@ -1,8 +1,23 @@
+import type { ErrorType } from '../types/error.types'
+
 export class SDKError extends Error {
   provider: string;
+  code: string; 
 
-  constructor(message: string, provider: string) {
+  constructor(message: string, provider: string, code: string) {
     super(message);
+    
+    this.name = 'SDKError';
     this.provider = provider;
+    this.code = code;
+  }
+
+  toErrorType(): ErrorType {
+    return {
+      name: this.name,
+      provider: this.provider,
+      code: this.code,
+      message: this.message,
+    };
   }
 }
---
+++ src/types/error.types.ts
@@ -0,0 +1,6 @@
+export type ErrorType = {
+  name: string;
+  provider: string;
+  code: string;
+  message: string;
+}
